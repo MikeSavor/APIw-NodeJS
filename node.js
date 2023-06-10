6d838f014ea1fa49f7a0fc42a77bb854867522ac
MikeSavor <tennismich@gmail.com>
2023-06-10 17:54:37 +0300

    Express REST server / 4 routes

--- node.js
+++ node.js
@@ -1,9 +1,49 @@
-const http = require("http");
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
 
-const PORT = 5000;
+app.use(bodyParser.json())
 
-const server = http.createServer(async (req, res) => {});
+//Create User (Getting data through json response)
+app.post('/CreateUser/', function(req, res){
+    var data = req.body
+    var name = data.name
+    var email = data.email
+    var password =data.password
+})
 
-server.listen(PORT, () => {
-    console.log(`HELLO WORLD`);
-});
+//Retrieve users
+app.get('/GetUsers', function(req, res){
+    res.send('Users')
+})
+
+//Update users 
+app.put('/UpdateUser/:username/:newpassword?/:newemail?/:newusername?', function(req, res){
+    var username = req.params.username
+    var newpassword = req.params.newpassword
+    var newemail = req.params.newemail
+    var newusername = req.params.newusername
+
+    if(newpassword){
+        //update user's password
+    }
+    else if (newemail){
+        //update user's email
+    }
+    else if (newusername){
+        //update user's username
+    }
+})
+
+//Delete users
+app.delete('/DeleteUser/:username', function(req, res){
+    var username = req.params.username
+    //delete user with username
+})
+
+var server = app.listen(8081, '127.0.0.1' ,function(){
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s",host, port)
+})
+
