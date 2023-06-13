17cf62070ed9f9fdec8cf5e6a168fdfca5c47af6
MikeSavor <tennismich@gmail.com>
2023-06-13 20:04:35 +0300

    POST now sends data to a local MongoDB database

--- node.js
+++ node.js
@@ -1,8 +1,17 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose');
 
 app.use(bodyParser.json())
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+  }, { collection: 'Users' });
+  
+const User = mongoose.model('User', userSchema);
 
 //Create User (Getting data through json response)
 app.post('/CreateUser/', function(req, res){
@@ -10,15 +19,32 @@ app.post('/CreateUser/', function(req, res){
     var name = data.name
     var email = data.email
     var password =data.password
+
+    const newUser = new User({
+        name: name,
+        email: email,
+        password: password
+      });
+
+      newUser.save()
+    .then(() => {
+        console.log('User: '+name+' saved successfully');
+        res.send(200)
+    })
+    .catch((error) => {
+        console.error('Error saving user:', error);
+        res.send(500)
+    });
 })
 
-//Retrieve users
-app.get('/GetUsers', function(req, res){
+//Retrieve user with a specific username
+app.get('/username',function(req, res){
     res.send('Users')
+    
 })
 
 //Update users 
-app.put('/UpdateUser/:username/:newpassword?/:newemail?/:newusername?', function(req, res){
+app.put('/:username/:newpassword?/:newemail?/:newusername?', function(req, res){
     var username = req.params.username
     var newpassword = req.params.newpassword
     var newemail = req.params.newemail
@@ -36,7 +62,7 @@ app.put('/UpdateUser/:username/:newpassword?/:newemail?/:newusername?', function
 })
 
 //Delete users
-app.delete('/DeleteUser/:username', function(req, res){
+app.delete('/:username', function(req, res){
     var username = req.params.username
     //delete user with username
 })
@@ -47,3 +73,15 @@ var server = app.listen(8081, '127.0.0.1' ,function(){
     console.log("Example app listening at http://%s:%s",host, port)
 })
 
+//mongodb+srv://tennismich:<password>@somecluster.jkyvdov.mongodb.net/?retryWrites=true&w=majority
+
+async function connectToDb(){
+    try{
+        await mongoose.connect('mongodb://localhost:27017/Users')
+        console.log("Connected to database")
+    }catch{
+        console.log("Couldnt connect to database")
+    }
+}
+
+connectToDb()
